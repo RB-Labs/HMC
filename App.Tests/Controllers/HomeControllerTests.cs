163f5eb9e486763c50ee2f3c656a7ed41abfb671
AndreyBuyanov <andreew.12.87@gmail.com>
2021-04-27 12:00:11 +0700

    Add HomeController tests

---
+++ App.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xunit;
+using App.Data;
+using App.Models;
+using App.Controllers;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace App.Tests.Controllers
+{
+    public class HomeControllerTests
+    {
+        private ILogger<HomeController> _logger;
+        public HomeControllerTests()
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddLogging()
+                .BuildServiceProvider();
+            var factory = serviceProvider.GetService<ILoggerFactory>();
+            _logger = factory.CreateLogger<HomeController>();
+        }
+        [Fact]
+        public void IndexViewResultNotNull()
+        {
+            HomeController controller = new HomeController(_logger);
+            ViewResult result = controller.Index() as ViewResult;
+            Assert.NotNull(result);
+        }
+    }
+}
