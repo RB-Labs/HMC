290496f59fefe2b9660116ecad7b3706bf8848f7
arusskina <arusskina9@gmail.com>
2021-04-28 11:08:58 +0700

    Add User tests

--- App.Tests/Models/UserTests.cs
+++ App.Tests/Models/UserTests.cs
@@ -26,7 +26,7 @@ namespace App.Tests.Models
         }
 
         [Fact]
-        public async void AddUserTest()
+        public async void CreateUserTest()
         {
             var addResult = _context.Users.Add(new User
                 {
@@ -40,6 +40,54 @@ namespace App.Tests.Models
                 .FirstOrDefaultAsync(m => m.UserName == "User1");
             Assert.NotNull(user);
             Assert.Equal("user1@mail.com", user.Email);
+        }
+
+        [Fact]
+        public async void UpdateUserTest()
+        {
+            var addResult = _context.Users.Add(new User
+            {
+                UserName = "User2",
+                Email = "user2@mail.com"
+            });
+            Assert.NotNull(addResult);
+            int entitiesCount = _context.SaveChanges();
+            Assert.Equal(1, entitiesCount);
+            var user = await _context.Users
+                .FirstOrDefaultAsync(m => m.UserName == "User2");
+            Assert.NotNull(user);
+            Assert.Equal("user2@mail.com", user.Email);
+            user.Email = "user2-new@mail.com";
+            var updateResult = _context.Users.Update(user);
+            Assert.NotNull(updateResult);
+            user = await _context.Users
+                .FirstOrDefaultAsync(m => m.UserName == "User2");
+            Assert.NotNull(user);
+            Assert.Equal("user2-new@mail.com", user.Email);
+        }
+
+        [Fact]
+        public async void DeleteUserTest()
+        {
+            var addResult = _context.Users.Add(new User
+            {
+                UserName = "User3",
+                Email = "user3@mail.com"
+            });
+            Assert.NotNull(addResult);
+            int entitiesCount = _context.SaveChanges();
+            Assert.Equal(1, entitiesCount);
+            var user = await _context.Users
+                .FirstOrDefaultAsync(m => m.UserName == "User3");
+            Assert.NotNull(user);
+            Assert.Equal("user3@mail.com", user.Email);
+            var removeResult = _context.Users.Remove(user);
+            Assert.NotNull(removeResult);
+            entitiesCount = _context.SaveChanges();
+            Assert.Equal(1, entitiesCount);
+            user = await _context.Users
+                .FirstOrDefaultAsync(m => m.UserName == "User3");
+            Assert.Null(user);
         }
 
         public void Dispose()
