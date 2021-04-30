a0f2e7dbe85ab88ef4ff44d54d25d269855c1975
arusskina <arusskina9@gmail.com>
2021-04-30 20:36:14 +0700

    Add ArticleCategoriesController and corresponding views

---
+++ App/Controllers/ArticleCategoriesController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using App.Data;
+using App.Models;
+
+namespace App.Controllers
+{
+    public class ArticleCategoriesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ArticleCategoriesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.ArticleCategories.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var articleCategory = await _context.ArticleCategories
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (articleCategory == null)
+            {
+                return NotFound();
+            }
+
+            return View(articleCategory);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name")] ArticleCategory articleCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(articleCategory);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(articleCategory);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var articleCategory = await _context.ArticleCategories.FindAsync(id);
+            if (articleCategory == null)
+            {
+                return NotFound();
+            }
+            return View(articleCategory);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] ArticleCategory articleCategory)
+        {
+            if (id != articleCategory.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(articleCategory);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ArticleCategoryExists(articleCategory.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(articleCategory);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var articleCategory = await _context.ArticleCategories
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (articleCategory == null)
+            {
+                return NotFound();
+            }
+
+            return View(articleCategory);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var articleCategory = await _context.ArticleCategories.FindAsync(id);
+            _context.ArticleCategories.Remove(articleCategory);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ArticleCategoryExists(int id)
+        {
+            return _context.ArticleCategories.Any(e => e.Id == id);
+        }
+    }
+}
