3623022c5036f40785117c0c91f117dd2243c87e
arusskina <arusskina9@gmail.com>
2021-05-01 22:11:21 +0700

    Add Article

---
+++ App/Models/Article.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace App.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Text { get; set; }
+        public ArticleCategory Category { get; set; }
+        public User Author { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
