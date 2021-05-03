ecb54200cc1c633094bef6b51d3a717bb0c4ca41
AndreyBuyanov <andreew.12.87@gmail.com>
2021-05-03 20:51:20 +0700

    Add UserRolesController and corresponding views

---
+++ App/Areas/Admin/Controllers/UserRolesController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using App.Data;
+using App.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using App.ViewModels;
+
+namespace App.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = UserRole.Admin)]
+    public class UserRolesController : Controller
+    {
+        RoleManager<UserRole> _roleManager;
+        UserManager<User> _userManager;
+
+        public UserRolesController(RoleManager<UserRole> roleManager, UserManager<User> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _userManager.Users.ToListAsync());
+        }
+
+        public async Task<IActionResult> Edit(string userId)
+        {
+            User user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var userRoles = await _userManager.GetRolesAsync(user);
+            var allRoles = _roleManager.Roles.ToList();
+            ChangeRoleViewModel model = new ChangeRoleViewModel
+            {
+                UserId = user.Id,
+                UserEmail = user.Email,
+                UserRoles = userRoles,
+                AllRoles = allRoles
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string userId, List<string> roles)
+        {
+            User user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var userRoles = await _userManager.GetRolesAsync(user);
+            var allRoles = _roleManager.Roles.ToList();
+            var addedRoles = roles.Except(userRoles);
+            var removedRoles = userRoles.Except(roles);
+
+            await _userManager.AddToRolesAsync(user, addedRoles);
+            await _userManager.RemoveFromRolesAsync(user, removedRoles);
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ App/ViewModels/ChangeRoleViewModel.cs
@@ -0,0 +1,20 @@
+﻿using App.Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace App.ViewModels
+{
+    public class ChangeRoleViewModel
+    {
+        public ChangeRoleViewModel()
+        {
+            AllRoles = new List<UserRole>();
+            UserRoles = new List<string>();
+        }
+        public string UserId { get; set; }
+        [Display(Name = "User Email")]
+        public string UserEmail { get; set; }
+        public List<UserRole> AllRoles { get; set; }
+        public IList<string> UserRoles { get; set; }
+    }
+}
