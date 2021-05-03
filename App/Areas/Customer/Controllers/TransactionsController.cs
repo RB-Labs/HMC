26a8d9ea7909bed850afd13aa5c345d49e76ab0c
arusskina <arusskina9@gmail.com>
2021-05-03 23:46:34 +0700

    Add TransactionsController to Customer area

---
+++ App/Areas/Customer/Controllers/TransactionsController.cs
@@ -0,0 +1,54 @@
+﻿using App.Data;
+using App.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    [Authorize(Roles = UserRole.Customer)]
+    public class TransactionsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TransactionsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var transactions = await _context.Transactions
+                .Include(x => x.Customer)
+                .Where(x => x.Customer.Email == User.Identity.Name)
+                .ToListAsync();
+            ViewBag.Balance = transactions.Sum(x => x.Value);
+            return View(transactions);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("Value,Description")] Transaction transaction)
+        {
+            if (ModelState.IsValid)
+            {
+                transaction.Customer = _context.Users
+                    .Single(x => x.Email == User.Identity.Name);
+                transaction.Date = DateTime.Now;
+                _context.Add(transaction);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(transaction);
+        }
+    }
+}
