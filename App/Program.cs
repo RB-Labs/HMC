5ca2346736ce687e48b33381df428da67e9a8143
AndreyBuyanov <andreew.12.87@gmail.com>
2021-05-05 18:40:51 +0700

    Remove unused dependencies

--- App/Program.cs
+++ App/Program.cs
@@ -1,11 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using App.Models;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Threading.Tasks;
 
 namespace App
 {
